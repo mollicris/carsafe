be650647d73a0cdd7e410c5b6ec3ed3bb1484831
cmollinedo <cristianm184@gmail.com>
2025-08-31 05:24:57 -0400

    warinng

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import React, { useRef } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { hardhat } from "viem/chains";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
@@ -27,7 +26,7 @@ export const HeaderMenuLinks = () => {
 
   return (
     <>
-      {menuLinks.map(({ label, href, icon }) => {
+      {menuLinks.map(({ label, href }) => {
         const isActive = pathname === href;
         return (
           <li key={href}>
@@ -37,7 +36,7 @@ export const HeaderMenuLinks = () => {
               className={`${isActive ? "bg-secondary shadow-md" : ""
                 } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
             >
-              {icon}
+
               <span>{label}</span>
             </Link>
           </li>
